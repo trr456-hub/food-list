353f0900e1ef441cb6333e48e9c2a9d1120ee62f
seokgeun <trr456@naver.com>
2023-01-16 22:47:05 +0900

    modal로 넘어갈때 디스트럭처링 딥카피 하여 각각의 모달창 띄우기 구현 db전부넣기 구현

--- src/components/FoodModal.js
+++ src/components/FoodModal.js
@@ -1,7 +1,7 @@
 import { useEffect, useRef } from "react";
 
-const FoodModal = ({ setModal, foodData }) => {
-  //console.log(foodData);
+const FoodModal = ({ modalState, setModal, foodData }) => {
+  console.log(modalState);
   const closeModal = () => {
     setModal(false);
   };
@@ -28,35 +28,35 @@ const FoodModal = ({ setModal, foodData }) => {
         ❌
       </button>
       <div ref={modalRef}>
-        <img src={foodData.ATT_FILE_NO_MAIN} alt="imges" />
-        <h2>{foodData.RCP_NM}</h2>
+        <img src={foodData[modalState].ATT_FILE_NO_MAIN} alt="imges" />
+        <h2>{foodData[modalState].RCP_NM}</h2>
         <div>
           <div>
             <h2>영양정보</h2>
-            <span>칼로리 : {foodData.INFO_ENG} </span>
-            <span>탄수화물 : {foodData.INFO_CAR} </span>
-            <span>단백질 : {foodData.INFO_PRO} </span>
-            <span>지방 : {foodData.INFO_FAT} </span>
-            <span>나트륨: {foodData.INFO_NA} </span>
+            <span>칼로리 : {foodData[modalState].INFO_ENG} </span>
+            <span>탄수화물 : {foodData[modalState].INFO_CAR} </span>
+            <span>단백질 : {foodData[modalState].INFO_PRO} </span>
+            <span>지방 : {foodData[modalState].INFO_FAT} </span>
+            <span>나트륨: {foodData[modalState].INFO_NA} </span>
           </div>
           <div>
             <h2>재료</h2>
-            <div>{foodData.RCP_PARTS_DTLS}</div>
+            <div>{foodData[modalState].RCP_PARTS_DTLS}</div>
           </div>
           <div>
             <h2>조리방법</h2>
-            <div>{foodData.MANUAL01}</div>
-            <div>{foodData.MANUAL02}</div>
-            <div>{foodData.MANUAL03}</div>
-            <div>{foodData.MANUAL04}</div>
-            <div>{foodData.MANUAL05}</div>
-            <div>{foodData.MANUAL06}</div>
-            <div>{foodData.MANUAL07}</div>
-            <div>{foodData.MANUAL08}</div>
-            <div>{foodData.MANUAL09}</div>
-            <div>{foodData.MANUAL10}</div>
-            <div>{foodData.MANUAL11}</div>
-            <div>{foodData.MANUAL12}</div>
+            <div>{foodData[modalState].MANUAL01}</div>
+            <div>{foodData[modalState].MANUAL02}</div>
+            <div>{foodData[modalState].MANUAL03}</div>
+            <div>{foodData[modalState].MANUAL04}</div>
+            <div>{foodData[modalState].MANUAL05}</div>
+            <div>{foodData[modalState].MANUAL06}</div>
+            <div>{foodData[modalState].MANUAL07}</div>
+            <div>{foodData[modalState].MANUAL08}</div>
+            <div>{foodData[modalState].MANUAL09}</div>
+            <div>{foodData[modalState].MANUAL10}</div>
+            <div>{foodData[modalState].MANUAL11}</div>
+            <div>{foodData[modalState].MANUAL12}</div>
           </div>
         </div>
       </div>
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -14,7 +14,14 @@ const Home = () => {
   const [gu, setGu] = useState("");
   const [ric, setRic] = useState("");
   const [modal, setModal] = useState(false);
-  const [data, setData] = useState([]);
+  // const [data, setData] = useState({});
+  const [modalData, setModalData] = useState({
+    ban: null,
+    ilp: null,
+    desser: null,
+    gu: null,
+    ric: null,
+  });
   const getFoods = async () => {
     const json = await (
       await fetch(
@@ -38,24 +45,30 @@ const Home = () => {
   // State 의 변화 => 렌더링 => 컴포넌트 내부 변수들 초기화
   // Ref 의 변화 => NO렌더링 => 변수들의 값이 유지됨
   // State 의 변화 => 렌더링 => 그래도 Ref의 값은 유지됨
-
+  const keyToKorean = {
+    ban: "반찬",
+    ilp: "일품",
+    desser: "후식",
+    gu: "국&찌개",
+    ric: "밥",
+  };
   /** array의 길이만큼에 숫자 중에서 랜덤으로 숫자 하나를 뽑는 함수 */
   const randomNum = (arr) => Math.floor(Math.random() * arr.length);
   /** state 중에서 setter 들만 모아놓은 object */
   const setters = {
-    반찬: setBan,
-    일품: setIlp,
-    후식: setDesser,
-    "국&찌개": setGu,
-    밥: setRic,
+    ban: setBan,
+    ilp: setIlp,
+    desser: setDesser,
+    gu: setGu,
+    ric: setRic,
   };
   /** state 중에서 getter 들만 모아놓은 object */
   const getters = {
-    반찬: ban,
-    일품: ilp,
-    후식: desser,
-    "국&찌개": gu,
-    밥: ric,
+    ban: ban,
+    ilp: ilp,
+    desser: desser,
+    gu: gu,
+    ric: ric,
   };
   /** 우리가 사용하는 음식종류들 ex: ["반찬","일품"...] */
   const kinds = Object.keys(setters);
@@ -63,39 +76,47 @@ const Home = () => {
   /** 버튼을 눌렀을때 kinds가 같은것들 로만 추출 */
   const showRdmFoods = (kind) => {
     //console.log(foods.filter((food) => food.RCP_PAT2 === "반찬"));
-    const foodsArr = foods.filter((food) => food.RCP_PAT2 === kind);
+    const foodsArr = foods.filter(
+      (food) => food.RCP_PAT2 === keyToKorean[kind]
+    );
     const randomFood = foodsArr[randomNum(foodsArr)];
     const foodComp = foodKinds(randomFood);
     setters[kind](foodComp);
-    setData(randomFood);
+    // setData((prevFood) => {
+    //   return { ...prevFood, ...randomFood };
+    // });
     //console.log(randomBan.props.children[0].props.children);
+    setModalData({ ...modalData, [kind]: randomFood });
+    // modaldata 원본 키값들을 변수처리화 한다.
+    // console.log(modalData);
   };
   useEffect(() => {
     //console.log(getFoods());
     getFoods();
   }, []);
   // modal 구현 탭
-  const setWindow = () => {
-    setModal(true);
-  };
+
   /** kinds 의 길이만큼 돌면서 음식종류별 버튼을 return하는 함수 */
   const kindBtns = kinds.map((kind, i) => (
     <div key={i}>
-      <button onClick={() => showRdmFoods(kind)}>{kind}추출</button>
-      <div onClick={setWindow}>{getters[kind]}</div>
+      <button onClick={() => showRdmFoods(kind)}>
+        {keyToKorean[kind]}추출
+      </button>
+      <div onClick={() => setModal(kind)}>{getters[kind]}</div>
     </div>
   ));
   // form 부분
-  const onSubmit = async (e) => {
-    e.preventDefault();
+  const onSubmit = async () => {
     try {
-      const docRef = await addDoc(collection(dbService, "food-list"), { data });
+      const docRef = await addDoc(collection(dbService, "food-list"), {
+        modalData,
+      });
       console.log("Document written with ID: ", docRef.id);
     } catch (error) {
       console.error("에러내용: ", error);
     }
   };
-  console.log(data);
+  //console.log(data);
   return (
     <div>
       {loading ? (
@@ -107,11 +128,17 @@ const Home = () => {
         // 리액트 라이브러리는 이 관계를 이용해 컴포넌트 리렌더링 여부를 결정한다.
         // 불필요한 리렌더링을 방지하기 위해서는 각 자식 컴포넌트마다 독립적인 key값을 넣어줘야 한다.
         <div>
-          <form onSubmit={onSubmit}>
-            {kindBtns}
-            {modal && <FoodModal setModal={setModal} foodData={data} />}
-            <input type="submit" value="전체기록" />
-          </form>
+          {/* <form onSubmit={onSubmit}> */}
+          {kindBtns}
+          {modal && (
+            <FoodModal
+              modalState={modal}
+              setModal={setModal}
+              foodData={modalData}
+            />
+          )}
+          <button onClick={onSubmit}>전체기록</button>
+          {/* </form> */}
         </div>
       )}
     </div>
