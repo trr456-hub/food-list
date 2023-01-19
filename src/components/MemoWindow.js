7b3074fadb5e34b9ba043df9650518701af47a70
gorhf9397 <trr456@naver.com>
2023-01-19 22:44:08 +0900

    memoWindow에서 객체의 key값을 배열화 하여 value값 도출후 뿌려주기 구현

--- src/components/MemoWindow.js
+++ src/components/MemoWindow.js
@@ -3,9 +3,20 @@ import { useLocation } from "react-router-dom";
 
 const MemoWindow = () => {
   const location = useLocation();
-  const foods = location.state.foodContents;
-  console.log(foods)
-  return <div>memorizeWindow</div>;
+  const foods = location.state.foodContents.foodData;
+  let foodsArr = Object.keys(foods);
+  let foodsMaping = foodsArr.map((item) => foods[item]);
+  console.log(foodsMaping);
+  return (
+    <div>
+      {foodsMaping.map((item) => (
+        <div key={item.RCP_SEQ}>
+          <div>{item.RCP_NM}</div>
+          <img src={item.ATT_FILE_NO_MAIN} />
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default MemoWindow;
